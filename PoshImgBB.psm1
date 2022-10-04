d5b00259607fa8d356b1014bcdf87d2c540d24b9
Largehawiian <jeremoyorama@gmail.com>
2022-10-04 07:51:53 -0400

    Added Windows Form Class

--- PoshImgBB.psm1
+++ PoshImgBB.psm1
@@ -2,6 +2,7 @@
 $PSScript = $PSScriptRoot
 $Func = @(Get-ChildItem -Recurse -Filter "*.ps1" -Path $PSScript -Exclude "ImageUploadScript.ps1" -ErrorAction Stop)
 Set-ExecutionPolicy -ExecutionPolicy Bypass process
+add-Type -AssemblyName System.Windows.Forms
 # Dotsourcing files
 ForEach ($import in $Func) {
     Try {
